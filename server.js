766dfcb0c7174deb3e8637c733c9dd1fba6ed566
Marc Cyr <marcacyr@gmail.com>
2015-01-17 21:38:21 -0800

    commit

---
+++ server.js
@@ -0,0 +1,37 @@
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var modulus     = require('./modulus.js');
+var port        = process.env.PORT || 8080;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
+
+app.use(morgan('dev'));
+
+app.get('/', function(req, res){
+  res.send('Welcome to the home page!');
+});
+
+var apiRouter = express.Router();
+
+apiRouter.get('/', function(req, res){
+  res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', apiRouter);
+
+mongoose.connect(localhost:27017/nodeApiDB);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
