bb26b5fd725a0eeedfaddeaac2f192eb353cc1b9
Marc Cyr <marcacyr@gmail.com>
2015-01-19 23:42:17 -0800

    everything up to the first steps of token auth. got tokens working

--- server.js
+++ server.js
@@ -3,11 +3,15 @@ var app         = express();
 var bodyParser  = require('body-parser');
 var morgan      = require('morgan');
 var mongoose    = require('mongoose');
+var User        = require('./app/models/user');
+var jwt         = require('jsonwebtoken');
 var port        = process.env.PORT || 8080;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+var superSecret = 'ilovescotchscotchyscotchscotch';
 
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -24,13 +28,106 @@ app.get('/', function(req, res){
 
 var apiRouter = express.Router();
 
+apiRouter.post('/authenticate', function(req, res){
+  User.findOne({
+    username: req.body.username
+  }).select('name username password').exec(function(err, user){
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.'});
+    } else if (user) {
+      var validPassword = user.comparePassword(req.body.password);
+      if (!validPassword) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.'})
+      } else {
+        var token = jwt.sign({
+          name: user.name,
+          username: user.username
+        }, superSecret, {
+          expiresInMinutes: 1440 //expires in 24 hours
+        });
+        res.json({
+          success: true,
+          message: 'Enjoy your token',
+          token: token
+        });
+      }
+    }
+  });
+});
+
+apiRouter.use(function(req, res, next){
+  console.log('Somebody just came to our app!');
+
+  next();
+});
+
 apiRouter.get('/', function(req, res){
   res.json({ message: 'hooray! welcome to our api!' });
 });
 
+apiRouter.route('/users')
+  .post(function(req, res){
+    var user = new User();
+
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+
+    user.save(function(err){
+      if (err){
+        if (err.code == 11000)
+          return res.json({ success: false, message: 'A user with that username already exists.'});
+        else
+          return res.send(err);
+      }
+        res.json({ message: 'User created!' });
+    });
+  })
+  .get(function(req, res){
+    User.find(function(err, users){
+      if (err) res.send(err);
+      res.json(users);
+    });
+  });
+
+apiRouter.route('/users/:user_id')
+  .get(function(req, res){
+    User.findById(req.params.user_id, function(err, user){
+      if (err) res.send(err);
+
+      res.json(user);
+    });
+  })
+  .put(function(req, res){
+    User.findById(req.params.user_id, function(err, user){
+      if (err) res.send(err);
+
+      if (req.body.name) user.name = req.body.name;
+      if (req.body.usernamd) user.username = req.body.username;
+      if (req.body.password) user.password = req.body.password;
+
+      user.save(function(err){
+        if (err) res.send(err);
+
+        res.json({ message: 'User updated!' });
+      });
+    });
+  })
+  .delete(function(req, res){
+    User.remove({
+      _id: req.params.user_id
+    }, function(err, user){
+      if (err) res.send(err);
+
+      res.json({ message: 'Successfully deleted' });
+    });
+  });
+
 app.use('/api', apiRouter);
 
-mongoose.connect('mongodb://localhost:27017/node_api');
+mongoose.connect('mongodb://marcacyr:mc109285@novus.modulusmongo.net:27017/vu3roSiv');
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
